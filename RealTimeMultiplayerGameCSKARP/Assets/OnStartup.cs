c47bcfb4ce7f64ac39f3822101f133bc2affd6d4
Lucas Juel <lucas.juel.sjostrom@gmail.com>
2024-02-28 15:56:05 +0100

    delete gitignore

--- RealTimeMultiplayerGameCSKARP/Assets/OnStartup.cs
+++ RealTimeMultiplayerGameCSKARP/Assets/OnStartup.cs
@@ -6,19 +6,25 @@ using UnityEngine;
 public class OnStartup : MonoBehaviour
 {   
     public GameObject obj;
-    public float speed;
+    private bool isOccupied = false;
+    MeshRenderer m_Render;
+    Color OriginalColor;
     // Start is called before the first frame update
     void Start()
     {
-        // Create a new instance of the object
-        float x = obj.transform.position.x;
-        float y = obj.transform.position.y;
+        m_Render = obj.GetComponent<MeshRenderer>();
+        OriginalColor = m_Render.material.color;
+    }
+    // Update is called once per frame
+    void OnMouseOver(){
+        if(isOccupied){
+            m_Render.material.color = Color.red;
+        } else {
+            m_Render.material.color = Color.green;
+        }
     }
 
-    // Update is called once per frame
-    void Update(){
-        float x = obj.transform.position.x;
-        obj.transform.position = new Vector3(x + speed, 0, 0);
-        
+    void OnMouseExit(){
+        m_Render.material.color = OriginalColor;
     }
 }

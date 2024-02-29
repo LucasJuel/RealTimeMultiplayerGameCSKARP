047eaf97cab84c1ce742fd57af47f189ff4bd265
Lucas Juel <lucas.juel.sjostrom@gmail.com>
2024-02-29 10:12:32 +0100

    add resource checks and text#

--- RealTimeMultiplayerGameCSKARP/Assets/Resources.cs
+++ RealTimeMultiplayerGameCSKARP/Assets/Resources.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Resources : MonoBehaviour
 {
     // Start is called before the first frame update
     public float money = 10000f;
+    public TMP_Text resourceText;
     void Start()
     {
         
@@ -14,7 +16,7 @@ public class Resources : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        resourceText.SetText("Money " + money);
     }
 
     public void setResource(float moneySpent){

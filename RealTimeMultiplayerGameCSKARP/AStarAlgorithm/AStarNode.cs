894fb322cf546d5571018460cdb38d3dd51b1dff
emilwandesen <emilwandersen@gmail.com>
2024-03-12 19:07:54 +0100

    AStarAlorithm

---
+++ RealTimeMultiplayerGameCSKARP/AStarAlgorithm/AStarNode.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class AStarNode{
+    //gCost = distance from the starting node
+    public int gCost;
+
+    //hCost = distance from the end node
+    public int hCost;
+
+    //fCost = gCost + hCost
+    public int fCost { get { return gCost + hCost; } }
+    
+    //parent = the node that this node came from
+    public AStarNode parent;
+
+    //isWalkable = whether or not the node can be walked on
+    public bool isWalkable;
+
+    //position = the position of the node in the grid
+    public Vector2Int position;
+
+    //toString = a string representation of the node
+    public string toString()
+    {
+        return "Node at position " + position.x + ", " + position.y;
+    }
+
+}
